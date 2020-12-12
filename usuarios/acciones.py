acee5a22680284400011b7aa4a170856fdf50ab4
luquiskywalker <lncorsich@gmail.com>
2020-12-12 17:13:11 -0300

    Generacion del login del usuario

--- usuarios/acciones.py
+++ usuarios/acciones.py
@@ -22,5 +22,22 @@ class Acciones:
     def login(self):
         print("\nOk, identificate en el sistema")
 
-        email = input("Introduce tu email: ")
-        password = input("Introduce una contraseña: ")
+        try:
+            email = input("Introduce tu email: ")
+            password = input("Introduce una contraseña: ")
+
+            usuario = modelo.Usuario("", "", email, password)
+            login = usuario.identificar()
+
+            if email == login[3]:
+                print(
+                    f"\nBienvenido {login[1]}, te has registrado en el sistema el {login[5]}")
+                self.proximasAcciones(login)
+        except Exception as e:
+            # print(type(e))
+            # print(type(e).__name__)
+            print(f"Login incorrecto, intenta nuevamente")
+
+
+def proximasAcciones(self, usuario):
+    return None
